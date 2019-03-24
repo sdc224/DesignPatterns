3b5ac8d030a681e9e6103b1cd1c9e94d6dd4aa01
Sourodeep Chatterjee <sourodeep224@outlook.com>
2019-03-24 17:15:11 +0530

    Added Singleton Pattern and publish

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="SDCWORLD">
+//   Sourodeep Chatterjee
+// </copyright>
+// <summary>
+//   Defines the Program type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace SingletonPattern
+{
+    using System;
+
+    /// <summary>
+    /// The program.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// The main.
+        /// </summary>
+        /// <param name="args">
+        /// The args.
+        /// </param>
+        public static void Main(string[] args)
+        {
+            var singleObjectInstance = SingleObject.GetSingleObjectInstance();
+            singleObjectInstance.ShowMessage();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SingletonPattern/SingleObject.cs
@@ -0,0 +1,50 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SingleObject.cs" company="SDCWORLD">
+//   Sourodeep Chatterjee
+// </copyright>
+// <summary>
+//   Defines the SingleObject type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace SingletonPattern
+{
+    using System;
+
+    /// <summary>
+    /// The single object.
+    /// </summary>
+    public class SingleObject
+    {
+        /// <summary>
+        /// The single object instance.
+        /// </summary>
+        private static readonly SingleObject SingleObjectInstance = new SingleObject();
+        
+        /// <summary>
+        /// Prevents a default instance of the <see cref="SingleObject"/> class from being created.
+        /// </summary>
+        private SingleObject()
+        {
+        }
+
+        /// <summary>
+        /// The get single object instance.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="SingleObject"/>.
+        /// </returns>
+        public static SingleObject GetSingleObjectInstance()
+        {
+            return SingleObjectInstance;
+        }
+
+        /// <summary>
+        /// The show message.
+        /// </summary>
+        public void ShowMessage()
+        {
+            Console.WriteLine("Hello from SingleObject class");
+        }
+    }
+}
